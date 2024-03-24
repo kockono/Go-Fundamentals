277f1000781f2bc8f44d0e43a358cc7ebf6e008c
kockono <kockonoop@gmail.com>
2024-03-24 10:21:04 -0600

    ✨ (events.go): Add routes for handling events including getting all events, getting a specific event, creating an event, and updating an event 📝 (routes.go): Create a new file to register routes for handling events in the server using Gin framework

---
+++ 11-rest-api/routes/events.go
@@ -0,0 +1,79 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"res-api.com/apis/models"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	// Permite retornar un JSON
+	context.JSON(http.StatusOK, events)
+
+}
+
+func getEvent(context *gin.Context) {
+	// Obtiene el parametro de la URL
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	event, err := models.GetEvent(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error:": err.Error()})
+		return // Exit the function
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"status": "Event created", "event": event})
+}
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var updateEvent models.Event
+	err = context.ShouldBindJSON(&updateEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updateEvent.ID = eventId
+	err = updateEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"status": "Event updated successfully!"})
+}
---
+++ 11-rest-api/routes/routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+	server.GET("/events/:id", getEvent)
+	server.PUT("/events/:id", updateEvent)
+}
