bde02329a24805187bf77ca5a29a396bac8fe8ed
kockono <kockonoop@gmail.com>
2024-03-24 10:20:50 -0600

    📝 (event.go): Update Event struct fields to improve semantics and add necessary imports for the file 🐛 (event.go): Modify Save method to return an error and handle database operations more efficiently 🐛 (event.go): Modify GetAllEvents method to return an error and handle database query execution more efficiently 🐛 (event.go): Modify GetEvent method to return an error and handle database query execution more efficiently 🐛 (event.go): Modify Update method to return an error and handle database update operation more efficiently

--- 11-rest-api/models/event.go
+++ 11-rest-api/models/event.go
@@ -1,10 +1,15 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"res-api.com/apis/db"
+)
 
 type Event struct {
-	ID          int       `binding:"required"`
-	Name        string    `binding:"required"`
+	ID          int64
+	Title       string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
 	DateTime    time.Time `binding:"required"`
@@ -14,11 +19,74 @@ type Event struct {
 var events []Event = []Event{}
 
 // Atteched to Event
-func (e Event) Save() {
-	// Save the event
-	events = append(events, e)
+func (e Event) Save() error {
+
+	query := `INSERT INTO events (title, description, location, date_time, user_id)
+	 VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+	// Close the statement when the function ends
+	defer stmt.Close()
+	// Execute the query Update, Create
+	result, err := stmt.Exec(e.Title, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * FROM events`
+	// Execute the query for get data
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var events []Event
+	// Iterate over the rows
+	for rows.Next() {
+		var event Event
+		// Scan the row and get the data
+		err := rows.Scan(&event.ID, &event.Title, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		fmt.Println(event)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func GetEvent(id int64) (Event, error) {
+	query := `SELECT * FROM events WHERE id = ?`
+	row := db.DB.QueryRow(query, id)
+	var event Event
+	err := row.Scan(&event.ID, &event.Title, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err != nil {
+		return event, err
+	}
+	return event, nil
+}
+
+func (event Event) Update() error {
+	query := `
+	UPDATE events 
+	SET title = ?, description = ?, location = ?, date_time = ?, user_id = ? 
+	WHERE id = ?`
+
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(event.Title, event.Description, event.Location, event.DateTime, event.UserID, event.ID)
+	return err
 }
