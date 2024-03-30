c8247337463686abb3559c7dcf04b5aed7947c55
kockono <kockonoop@gmail.com>
2024-03-30 00:28:17 -0600

    🐛 (jwt.go): Fix VerifyToken function to correctly extract userId from JWT claims and return it as int64 data type ✨ (jwt.go): Update token expiration time to 2 hours for better security and performance

--- 11-rest-api/utils/jwt.go
+++ 11-rest-api/utils/jwt.go
@@ -12,22 +12,24 @@ const secretKey = "supersecret"
 func GenerateToken(email string, userId int64) (string, error) {
 
 	// Encrypt and sign the token using the secret key
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email":  email,
 		"userId": userId,
-		"exp":    time.Now().Add(time.Hour * 24).Unix(),
+		"exp":    time.Now().Add(time.Hour * 2).Unix(),
 	})
 
 	// Sign and get the complete encoded token as a string using the secret
 	return token.SignedString([]byte(secretKey))
 }
 
-func VerifyToken(token string) error {
+func VerifyToken(token string) (int64, error) {
 	// Parse the token
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		// Check the signing method
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("unexpected signing method")
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Unexpected signing method")
 		}
 
 		return []byte(secretKey), nil
@@ -42,12 +44,13 @@ func VerifyToken(token string) error {
 	}
 
 	// Here we can extract the claims from the token
-	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
-	// if !ok {
-	// 	panic("Invalid claims")
-	// }
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		panic("Invalid claims")
+	}
 
 	// email := claims["email"].(string)
-	// userId := claims["userId"].(int64)
-	return nil
+	userId := int64(claims["userId"].(float64))
+
+	return userId, nil
 }
